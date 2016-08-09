b219ad598706bae7a1a9ee439d4be54ceb2b0097
Andrei Visan <andrei.m.visan@gmail.com>
2016-08-09 10:54:39 +0200

    beta

---
+++ app/src/main/java/adyen/com/cse/Card.java
@@ -0,0 +1,132 @@
+package adyen.com.cse;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import adyen.com.cse.encrypter.ClientSideEncrypter;
+import adyen.com.cse.encrypter.exception.EncrypterException;
+
+/**
+ * Created by andrei on 8/8/16.
+ */
+public class Card {
+
+    private static final String tag = Card.class.getSimpleName();
+
+    private String number;
+    private String expiryMonth;
+    private String expiryYear;
+    private String cardHolderName;
+    private String cvc;
+    private Date generationTime;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getExpiryMonth() {
+        return expiryMonth;
+    }
+
+    public void setExpiryMonth(String expiryMonth) {
+        this.expiryMonth = expiryMonth;
+    }
+
+    public String getExpiryYear() {
+        return expiryYear;
+    }
+
+    public void setExpiryYear(String expiryYear) {
+        this.expiryYear = expiryYear;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getCvc() {
+        return cvc;
+    }
+
+    public void setCvc(String cvc) {
+        this.cvc = cvc;
+    }
+
+    public Date getGenerationTime() {
+        return generationTime;
+    }
+
+    public void setGenerationTime(Date generationTime) {
+        this.generationTime = generationTime;
+    }
+
+    public String serialize(String publicKey) throws EncrypterException {
+        JSONObject cardJson = new JSONObject();
+        String encryptedData = null;
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        try {
+            cardJson.put("generationtime", simpleDateFormat.format(generationTime));
+            cardJson.put("number", number);
+            cardJson.put("holderName", cardHolderName);
+            cardJson.put("cvc", cvc);
+            cardJson.put("expiryMonth", expiryMonth);
+            cardJson.put("expiryYear", expiryYear);
+
+            encryptedData = encryptData(cardJson.toString(), publicKey);
+        } catch (JSONException e) {
+            Log.e(tag, e.getMessage(), e);
+        }
+
+        return encryptedData;
+    }
+
+    public String encryptData(String data, String publicKey) throws EncrypterException {
+        String encryptedData = null;
+
+        try {
+            ClientSideEncrypter encrypter = new ClientSideEncrypter(publicKey);
+            encryptedData = encrypter.encrypt(data);
+        } catch (EncrypterException e) {
+            throw e;
+        }
+
+        return encryptedData;
+    }
+
+    @Override
+    public String toString() {
+        JSONObject cardJson = new JSONObject();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        try {
+            cardJson.put("generationtime", simpleDateFormat.format(generationTime));
+            if (number.length() >= 4) {
+                cardJson.put("number", number.substring(0, 3));
+            }
+            cardJson.put("holderName", cardHolderName);
+        } catch (JSONException e) {
+            Log.e(tag, e.getMessage(), e);
+        }
+
+        return cardJson.toString();
+    }
+
+}
--- app/src/main/java/adyen/com/cse/MainActivity.java
+++ app/src/main/java/adyen/com/cse/MainActivity.java
@@ -3,13 +3,23 @@ package adyen.com.cse;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+
+import adyen.com.cse.encrypter.exception.EncrypterException;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mCardNumber;
     private EditText mCardExpiryDate;
     private EditText mCardCvc;
+    private TextView mCseResult;
 
     private String cardNumber;
     private String cardExpiryDate;
@@ -24,16 +34,44 @@ public class MainActivity extends AppCompatActivity {
         mCardNumber = (EditText)findViewById(R.id.credit_card_no);
         mCardExpiryDate = (EditText)findViewById(R.id.credit_card_exp_date);
         mCardCvc = (EditText)findViewById(R.id.credit_card_cvc);
-
-        initData();
+        mCseResult = (TextView)findViewById(R.id.cse_result);
     }
 
     private void initData() {
         cardNumber = mCardNumber.getText().toString();
         cardExpiryDate = mCardExpiryDate.getText().toString();
         cardCvc = mCardCvc.getText().toString();
+    }
 
-        Log.i("MainActivity|Card data", cardNumber + " | " + cardExpiryDate + " | " + cardCvc);
+    public void initPayment(View payButton) {
+        initData();
+        ConfigLoader configLoader = new ConfigLoader();
+        JSONObject configuration = configLoader.loadJsonConfiguration();
+        Card card = buildCardData();
+        try {
+            String encryptedCardData = card.serialize(configuration.getString("publicKey"));
+
+            Log.i("=======> ", "ecryptedCard=" + encryptedCardData);
+
+            mCseResult.setText(encryptedCardData);
+        } catch (EncrypterException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Card buildCardData() {
+        Card card = new Card();
+
+        card.setCardHolderName("test");
+        card.setCvc(cardCvc);
+        card.setExpiryMonth(cardExpiryDate.split("/")[0]);
+        card.setExpiryYear("20" + cardExpiryDate.split("/")[1]);
+        card.setGenerationTime(new Date());
+        card.setNumber(cardNumber);
+
+        return card;
     }
 
 
